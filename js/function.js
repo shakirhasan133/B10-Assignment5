5cb38a3b3eab3e8b5d20fb07f4311f597f20cfce
Shakir_Hasan <sakirhasan133@gmail.com>
2024-09-23 10:55:11 +0600

    Button Noakhali donation all work Done

--- js/function.js
+++ js/function.js
@@ -9,3 +9,39 @@ return document.getElementById(eId);
 function GetElementValueById(id){
     return document.getElementById(id).value;
 }
+
+function donationCalculate () {
+    const myTotalMoney = parseInt(getElementById('myTotalMoney').innerText);
+    return myTotalMoney;
+}
+
+function historyCreate(dTk, toDestination){
+    const historyTab =  getElementById('historyTab');
+    const Time = new Date();
+    const div = document.createElement('div');
+    div.classList.add('border', 'w-full', 'h-[100px]', 'rounded-lg', 'shadow-lg', 'p-5', 'mt-10');
+    div.innerHTML = `
+        <p class = "text-2xl font-bold"><span>${dTk}</span> is Donated For ${toDestination}</p>
+        <p class = "font-semibold">Date : ${Time}</p>
+    `;
+
+    historyTab.appendChild(div)
+
+}
+
+
+// setTimeOut for PopUp Modal 10 Second
+
+function popUpTimeout(){
+    setTimeout(()=>{
+        successPop.classList.add('hidden')
+    },10000)
+}
+
+// Validate Input Value 
+function checkInput(inputValue){
+    if (inputValue === isNaN || inputValue<0) {
+        alert("Please Enter a Valid Number")
+        return
+    }
+}
--- js/index.js
+++ js/index.js
@@ -4,9 +4,28 @@ const historyTabBtn = getElementById('historyTabBtn');
 const donationTabBtn = getElementById('donationTabBtn');
 const donationTab = getElementById('donationTab');
 const historyTab = getElementById('historyTab');
+const successPop = getElementById('successPop');
+const popup = document.getElementById('popup');
 
 
 
+
+
+// Donation Tab Button Work Here
+donationTabBtn.addEventListener('click', function(){
+    donationTabBtn.classList.add('bg-primary');
+    donationTabBtn.classList.remove('bg-white');
+    
+    historyTabBtn.classList.remove('bg-primary');
+    historyTabBtn.classList.add('bg-white');
+
+
+        // unhide Donation Tab Element
+        donationTab.classList.remove('hidden');
+
+        // hide history Tab
+        historyTab.classList.add('hidden');
+})
 
 
 
@@ -26,4 +45,54 @@ historyTabBtn.addEventListener('click', function(){
 
     // unhide history Tab
     historyTab.classList.remove('hidden');
+})
+
+
+
+
+// Calculate Button Work Here
+
+const btnNoakhali = document.getElementById('btnNoakhali');
+
+btnNoakhali.addEventListener('click', function(){
+    const titleNoakhali = getElementById('titleNoakhali').innerText;
+    const noakhaliDonationInputValue = getElementById('noakhaliInput');
+    const noakhaliDonationAmount = parseInt(getElementById('noakhaliDonationAmount').innerText);
+    const noakhaliInput = parseInt(GetElementValueById('noakhaliInput'));
+
+    if ( isNaN(noakhaliDonationInputValue.value) || noakhaliDonationInputValue.value<0) {
+        alert("Please Enter a Valid Number")
+        return
+    }
+ 
+    if (donationCalculate()>noakhaliInput) {
+
+        const nResult = noakhaliDonationAmount + noakhaliInput;
+        getElementById('noakhaliDonationAmount').innerHTML = nResult
+
+        const lastMoney = donationCalculate() - noakhaliInput;
+        getElementById('myTotalMoney').innerText = lastMoney;
+
+        historyCreate(noakhaliInput, titleNoakhali);
+
+
+
+
+        successPop.classList.remove('hidden')
+        popUpTimeout()
+        noakhaliDonationInputValue.value = '';
+        
+    }
+    else{
+        alert("insufficient Balance")
+    }
+
+  
+})
+
+
+// Close Pop Up
+
+getElementById('closePop').addEventListener('click', function(){
+    successPop.classList.add('hidden');
 })
